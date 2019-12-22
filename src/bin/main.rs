2cf22d9e3bfbf1ce979b88fb1ae5b22098908755
Lokathor <zefria@gmail.com>
2019-12-21 20:54:38 -0700

    we can print the types now.

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,18 +1,40 @@
 #![allow(unused)]
 
 use phosphorus::*;
+use XmlElement::*;
 
 fn main() {
   let bytes_vec = std::fs::read("xml/gl.xml").unwrap();
   let string = String::from_utf8(bytes_vec).unwrap();
   let body = drop_declaration(&string);
-  for elem in XmlIterator::new(body) {
-    println!("{:?}", elem);
+
+  let mut in_types = false;
+  let mut it = XmlIterator::new(body);
+  while let Some(elem) = it.next() {
     match elem {
-      XmlElement::StartTag { attrs, .. }
-      | XmlElement::EmptyTag { attrs, .. } => {
-        for (key, val) in AttributeIterator::new(attrs) {
-          println!("==k:{}, v:{}", key, val);
+      StartTag { name: "types", attrs: "" } => in_types = true,
+      StartTag { name: "type", attrs } if in_types => {
+        'type_walk: loop {
+          match it.next().unwrap() {
+            EndTag{ name: "type" } => {
+              println!();
+              println!();
+              break 'type_walk;
+            }
+            Text(text) => print!("{}", text),
+            EmptyTag { .. } => (),
+            StartTag{ name: "name", .. } => {
+              match it.next().unwrap() {
+                Text(text) => print!("{}", text),
+                other => panic!("unexpected>{:?}", other),
+              }
+              match it.next().unwrap() {
+                EndTag{ name: "name" } => (),
+                other => panic!("unexpected>{:?}", other),
+              }
+            }
+            other => panic!("unexpected>{:?}", other),
+          }
         }
       }
       _ => (),
