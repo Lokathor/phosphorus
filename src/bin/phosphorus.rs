df0fb6a29c227c756f80918b13160cf1a9d02aac
Lokathor <zefria@gmail.com>
2020-07-14 20:05:51 -0600

    .

--- src/bin/phosphorus.rs
+++ src/bin/phosphorus.rs
@@ -3,9 +3,10 @@
 use phosphorus::{ApiGroup, GlApiSelection, GlProfile, GlRegistry};
 
 fn main() {
+  const USAGE: &str = "Illegal Arg Count. Usage: phosphorus <filename> <api> <major> <minor> <profile> [comma,separated,extensions,if,any]";
   let args: Vec<_> = std::env::args_os().collect();
-  if args.len() != 7 {
-    panic!("Usage: phosphorus [filename] [api] [major] [minor] [profile] [extensions,list]");
+  if args.len() != 6 && args.len() != 7 {
+    panic!("Should be either 6 or 7 args. {}", USAGE);
   }
   let filename = &args[1];
   let api = match args[2].to_str().unwrap() {
@@ -13,25 +14,18 @@ fn main() {
     "gles1" => ApiGroup::Gles1,
     "gles2" => ApiGroup::Gles2,
     "glsc2" => ApiGroup::Glsc2,
-    _ => panic!("illegal api name, pick from [gl,gles1,gles2,glsc2]"),
+    _ => panic!("illegal api name, pick from {{gl,gles1,gles2,glsc2}}"),
   };
   let major: i32 = args[3].to_str().unwrap().parse().unwrap();
   let minor: i32 = args[4].to_str().unwrap().parse().unwrap();
   let profile = match args[5].to_str().unwrap() {
     "core" => GlProfile::Core,
     "compatibility" => GlProfile::Compatibility,
-    _ => panic!("illegal profile name, pick from [core,compatibility]"),
+    _ => panic!("illegal profile name, pick from {{core,compatibility}}"),
   };
-  let extensions: Vec<&str> =
-    args[6].to_str().unwrap().split(",").map(str::trim).collect();
+  let extensions: Vec<&str> = if args.len() == 7 { args[6].to_str().unwrap().split(",").map(str::trim).filter(|s| !s.is_empty()).collect() } else { Vec::new() };
   let gl_xml = std::fs::read_to_string(&filename).unwrap();
   let registry = GlRegistry::from_gl_xml_str(&gl_xml);
-  let selection = GlApiSelection::new_from_registry_api_extensions(
-    &registry,
-    api,
-    (major, minor),
-    profile,
-    &extensions,
-  );
+  let selection = GlApiSelection::new_from_registry_api_extensions(&registry, api, (major, minor), profile, &extensions);
   println!("{}", selection);
 }
