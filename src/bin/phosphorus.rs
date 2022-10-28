78d78b625a7935b0a7700d14c4e66ce664a3da31
Lokathor <zefria@gmail.com>
2022-10-27 20:11:44 -0600

    begin updates to new gl.xml

--- src/bin/phosphorus.rs
+++ src/bin/phosphorus.rs
@@ -1,276 +1,123 @@
-#![allow(unused)]
+#![allow(clippy::from_str_radix_10)]
+#![allow(clippy::field_reassign_with_default)]
 
-use magnesium::*;
-use phosphorus::{gl_extension_deltas::*, gl_feature_deltas::*, registry::*, source_managers::*, *};
-use std::collections::{BTreeSet, HashMap};
+use magnesium::{
+  skip_comments, skip_empty_text_elements, trim_text, ElementIterator,
+  TagAttributeIterator,
+  XmlElement::{self, *},
+};
 
 fn main() {
-  print_out_gl33_plus_extensions()
+  let mut args_list: Vec<String> = std::env::args().collect();
+
+  let file_name = if args_list.len() == 2 {
+    args_list.pop().unwrap()
+  } else {
+    println!("USAGE: phosphorus [gl.xml]");
+    return;
+  };
+  println!("Reading File: {file_name:?}");
+  let gl_xml = std::fs::read_to_string(file_name.as_str()).unwrap();
+  let mut elem_iter = ElementIterator::new(&gl_xml)
+    .filter_map(skip_comments)
+    .map(trim_text)
+    .filter_map(skip_empty_text_elements);
+  let registry = match elem_iter.next().unwrap() {
+    StartTag { name: "registry", attrs: "" } => Registry::new(&mut elem_iter),
+    _ => panic!("couldn't read the `registry` element"),
+  };
+  println!("registry: {registry:#?}");
 }
 
-fn print_out_registry() {
-  let args: Vec<String> = std::env::args().skip(1).collect();
-  let gl_xml_filename = &args[0];
-  if cfg!(debug_assertions) {
-    eprintln!("Reading `{}`", gl_xml_filename);
-  }
-  let gl_xml_string = std::fs::read_to_string(gl_xml_filename).expect("Couldn't read gl.xml file!");
-  if cfg!(debug_assertions) {
-    eprintln!("Read {} bytes.", gl_xml_string.len());
-  }
-  let gl_xml_str = if gl_xml_string.chars().nth(0).unwrap() == '\u{feff}' {
-    if cfg!(debug_assertions) {
-      eprintln!("Byte Order Mark detected, removing.");
-    }
-    &gl_xml_string['\u{feff}'.len_utf8()..]
-  } else {
-    &gl_xml_string
-  };
-  let mut iter = ElementIterator::new(&gl_xml_str).filter_map(skip_comments).filter_map(skip_empty_text_elements).map(trim_text);
-  let mut registry = Registry::from_element_iterator(&mut iter);
-  if cfg!(debug_assertions) {
-    eprintln!("Enumerations Count: {}", registry.enumerations.len());
-    eprintln!("Commands Count: {}", registry.commands.len());
-    eprintln!("Features Count: {}", registry.features.len());
-    eprintln!("Extensions Count: {}", registry.extensions.len());
-  }
-  let mut s = String::with_capacity(1024 * 1024 * 10);
-  println!("{}", s);
+#[derive(Debug, Clone, Default)]
+pub struct Registry {
+  pub enumerations: Vec<Enumeration>,
 }
-
-fn print_out_gl33_plus_extensions() {
-  // Read In The Registry
-  let args: Vec<String> = std::env::args().skip(1).collect();
-  let gl_xml_filename = &args[0];
-  if cfg!(debug_assertions) {
-    eprintln!("Reading `{}`", gl_xml_filename);
-  }
-  let gl_xml_string = std::fs::read_to_string(gl_xml_filename).expect("Couldn't read gl.xml file!");
-  if cfg!(debug_assertions) {
-    eprintln!("Read {} bytes.", gl_xml_string.len());
-  }
-  let gl_xml_str = if gl_xml_string.chars().nth(0).unwrap() == '\u{feff}' {
-    if cfg!(debug_assertions) {
-      eprintln!("Byte Order Mark detected, removing.");
-    }
-    &gl_xml_string['\u{feff}'.len_utf8()..]
-  } else {
-    &gl_xml_string
-  };
-  let mut iter = ElementIterator::new(&gl_xml_str).filter_map(skip_comments).filter_map(skip_empty_text_elements).map(trim_text);
-  let mut registry = Registry::from_element_iterator(&mut iter);
-
-  // Prepare Our Api To Output
-  let mut enums_needed = BTreeSet::new();
-  let mut non_null_commands = BTreeSet::new();
-  let mut nullable_commands = BTreeSet::new();
-  for feature_delta in [GL_VERSION_1_0, GL_VERSION_1_1, GL_VERSION_1_2, GL_VERSION_1_3, GL_VERSION_1_4, GL_VERSION_1_5, GL_VERSION_2_0, GL_VERSION_2_1, GL_VERSION_3_0, GL_VERSION_3_1, GL_VERSION_3_2, GL_VERSION_3_3].iter() {
-    for enum_name in feature_delta.enums_added.iter().copied() {
-      enums_needed.insert(enum_name);
-    }
-    // core commands are required.
-    for command_name in feature_delta.commands_added.iter().copied() {
-      non_null_commands.insert(command_name);
-    }
-    // Only the 3.2 level *actually* removes stuff
-    for enum_name in feature_delta.enums_removed.iter().copied() {
-      enums_needed.remove(enum_name);
-    }
-    for command_name in feature_delta.commands_removed.iter().copied() {
-      non_null_commands.remove(command_name);
-    }
-  }
-  for extension_delta in [GL_KHR_debug_GL, GL_ARB_texture_filter_anisotropic, GL_ARB_pipeline_statistics_query].iter() {
-    for enum_name in extension_delta.enums_added.iter().copied() {
-      enums_needed.insert(enum_name);
-    }
-    // extension commands are nullable
-    for command_name in extension_delta.commands_added.iter().copied() {
-      nullable_commands.insert(command_name);
-    }
-  }
-  let mut enumeration_entries = get_enumeration_entries();
-  enumeration_entries.retain(|ee| enums_needed.contains(ee.name.as_str()));
-  let mut command_type_entries = get_command_type_entries();
-  command_type_entries.retain(|cte| non_null_commands.contains(cte.name.as_str()) || nullable_commands.contains(cte.name.as_str()));
-
-  // Print All Enumeration Declarations
-  for enum_entry in enumeration_entries.iter() {
-    print!("{}", enum_entry.comments);
-    println!("{}", enum_entry.enum_declaration);
-    println!();
-  }
-
-  println!();
-  println!("// // // // // // // // // // // // // // // // // // // // // // // // // //");
-  println!();
-
-  // Print All Group Declarations
-  let mut g: HashMap<&str, Vec<&str>> = HashMap::new();
-  for enumeration in registry.enumerations.iter().filter(|e| enums_needed.contains(e.name)) {
-    for group in enumeration.group.as_ref().unwrap_or(&"").split(',') {
-      if group == "" {
-        continue;
+impl Registry {
+  pub fn new<'a>(mut elem_iter: impl Iterator<Item = XmlElement<'a>>) -> Self {
+    let mut reg = Self::default();
+    loop {
+      match elem_iter.next().unwrap() {
+        EndTag { name: "registry" } => return reg,
+        StartTag { name: "comment", attrs: "" } => {
+          // for some goofy reason, gl.xml has comments in matched comment tags
+          // as well as in the usual `<!--` and `-->`
+          burn_until("comment", &mut elem_iter)
+        }
+        StartTag { name: "types", attrs: "" } => {
+          // We won't bother to read the `types` tree at all. It's badly
+          // formatted because it contains literal C code and junk. Instead, see
+          // the `gl_core_types` module.
+          burn_until("types", &mut elem_iter)
+        }
+        EmptyTag { name: "enums", attrs: _ } => {
+          // we don't need to keep empty `enums` instances, since they don't
+          // hold any enum values.
+        }
+        StartTag { name: "enums", attrs } => 'enums: loop {
+          let is_bitmask = TagAttributeIterator::new(attrs)
+            .any(|attr| attr.key == "type" && attr.value == "bitmask");
+          match elem_iter.next().unwrap() {
+            EndTag { name: "enums" } => break 'enums,
+            EmptyTag { name: "unused", attrs: _ } => (),
+            EmptyTag { name: "enum", attrs } => {
+              let mut enumeration = Enumeration::default();
+              enumeration.is_bitmask = is_bitmask;
+              for attr in TagAttributeIterator::new(attrs) {
+                match attr.key {
+                  "value" => enumeration.value = attr.value.to_string(),
+                  "name" => enumeration.name = attr.value.to_string(),
+                  "group" => enumeration.group = attr.value.to_string(),
+                  "alias" => enumeration.alias = Some(attr.value.to_string()),
+                  "type" => enumeration.type_ = Some(attr.value.to_string()),
+                  "api" => enumeration.api = Some(attr.value.to_string()),
+                  "comment" => (),
+                  other => panic!("{other:?}"),
+                }
+              }
+              reg.enumerations.push(enumeration);
+            }
+            other => panic!("{other:?}"),
+          }
+        },
+        StartTag { name: "commands", attrs: r#"namespace="GL""# } => {
+          burn_until("commands", &mut elem_iter)
+        }
+        StartTag { name: "feature", attrs } => {
+          println!("feature_attrs: {attrs:?}");
+          burn_until("feature", &mut elem_iter)
+        }
+        StartTag { name: "extensions", attrs: "" } => {
+          burn_until("extensions", &mut elem_iter)
+        }
+        other => {
+          println!("UNKNOWN: Registry: {other:?}");
+          return reg;
+        }
       }
-      g.entry(group).or_insert(Vec::new()).push(enumeration.name);
     }
   }
-  let mut group_list: Vec<&str> = g.keys().copied().filter(|g| command_type_entries.iter().any(|cte| cte.type_declaration.contains(g))).collect();
-  group_list.sort();
-  for group in group_list.iter() {
-    g.entry(group).or_insert(Vec::new()).sort();
-    println!("/// {}", group);
-    for group_entry in g.entry(group).or_insert(Vec::new()).iter() {
-      println!("/// * [`{}`]", group_entry);
-    }
-    println!("pub type {} = GLenum;", group);
-    println!("");
-  }
-
-  println!();
-  println!("// // // // // // // // // // // // // // // // // // // // // // // // // //");
-  println!();
-
-  // Print All Command Type Declarations
-  for command_type_entry in command_type_entries.iter() {
-    // these types are crate-private and the docs for each command appear on the
-    // struct method.
-    println!("pub(crate) {}", &command_type_entry.type_declaration["pub ".len()..]);
-    println!();
-  }
-
-  println!();
-  println!("// // // // // // // // // // // // // // // // // // // // // // // // // //");
-  println!();
-  // print command loader
-  let non_null_commands: Vec<&str> = non_null_commands.iter().copied().collect();
-  let nullable_commands: Vec<&str> = nullable_commands.iter().copied().collect();
-  let mut s = String::with_capacity(10 * 1024 * 1024);
-  fmt_struct_loader(&mut s, "GlFns", &non_null_commands, &nullable_commands);
-  println!("{}", s);
-
-  println!();
-  println!("// // // // // // // // // // // // // // // // // // // // // // // // // //");
-  println!();
-  // print command loader
-  let mut nullable_global_commands: Vec<&str> = nullable_commands.clone();
-  nullable_global_commands.extend(non_null_commands);
-  nullable_global_commands.sort();
-  let mut s = String::with_capacity(10 * 1024 * 1024);
-  fmt_global_loader(&mut s, &nullable_global_commands);
-  println!("{}", s);
 }
 
-fn print_out_gl46_superbible() {
-  // Read In The Registry
-  let args: Vec<String> = std::env::args().skip(1).collect();
-  let gl_xml_filename = &args[0];
-  if cfg!(debug_assertions) {
-    eprintln!("Reading `{}`", gl_xml_filename);
-  }
-  let gl_xml_string = std::fs::read_to_string(gl_xml_filename).expect("Couldn't read gl.xml file!");
-  if cfg!(debug_assertions) {
-    eprintln!("Read {} bytes.", gl_xml_string.len());
-  }
-  let gl_xml_str = if gl_xml_string.chars().nth(0).unwrap() == '\u{feff}' {
-    if cfg!(debug_assertions) {
-      eprintln!("Byte Order Mark detected, removing.");
-    }
-    &gl_xml_string['\u{feff}'.len_utf8()..]
-  } else {
-    &gl_xml_string
-  };
-  let mut iter = ElementIterator::new(&gl_xml_str).filter_map(skip_comments).filter_map(skip_empty_text_elements).map(trim_text);
-  let mut registry = Registry::from_element_iterator(&mut iter);
+#[derive(Debug, Clone, Default)]
+pub struct Enumeration {
+  pub value: String,
+  pub name: String,
+  pub group: String,
+  pub alias: Option<String>,
+  pub type_: Option<String>,
+  pub api: Option<String>,
+  pub is_bitmask: bool,
+}
 
-  // Prepare Our Api To Output
-  let mut enums_needed = BTreeSet::new();
-  let mut non_null_commands = BTreeSet::new();
-  let mut nullable_commands = BTreeSet::new();
-  for feature_delta in [GL_VERSION_1_0, GL_VERSION_1_1, GL_VERSION_1_2, GL_VERSION_1_3, GL_VERSION_1_4, GL_VERSION_1_5, GL_VERSION_2_0, GL_VERSION_2_1, GL_VERSION_3_0, GL_VERSION_3_1, GL_VERSION_3_2, GL_VERSION_3_3, GL_VERSION_4_0, GL_VERSION_4_1, GL_VERSION_4_2, GL_VERSION_4_3, GL_VERSION_4_4, GL_VERSION_4_5, GL_VERSION_4_6].iter() {
-    for enum_name in feature_delta.enums_added.iter().copied() {
-      enums_needed.insert(enum_name);
-    }
-    // core commands are required.
-    for command_name in feature_delta.commands_added.iter().copied() {
-      non_null_commands.insert(command_name);
-    }
-    // Only the 3.2 level *actually* removes stuff
-    for enum_name in feature_delta.enums_removed.iter().copied() {
-      enums_needed.remove(enum_name);
-    }
-    for command_name in feature_delta.commands_removed.iter().copied() {
-      non_null_commands.remove(command_name);
-    }
-  }
-  // anisotropic is just nifty, and doesn't take up any function pointers. The
-  // rest of these are used by The OpenGL SuperBible 7e
-  for extension_delta in [GL_ARB_texture_filter_anisotropic, GL_ARB_bindless_texture, GL_ARB_sparse_texture, GL_ARB_pipeline_statistics_query].iter() {
-    for enum_name in extension_delta.enums_added.iter().copied() {
-      enums_needed.insert(enum_name);
-    }
-    // extension commands are nullable
-    for command_name in extension_delta.commands_added.iter().copied() {
-      nullable_commands.insert(command_name);
-    }
-  }
-  let mut enumeration_entries = get_enumeration_entries();
-  enumeration_entries.retain(|ee| enums_needed.contains(ee.name.as_str()));
-  let mut command_type_entries = get_command_type_entries();
-  command_type_entries.retain(|cte| non_null_commands.contains(cte.name.as_str()) || nullable_commands.contains(cte.name.as_str()));
-
-  // Print All Enumeration Declarations
-  //for enum_entry in enumeration_entries.iter() {
-  //  print!("{}", enum_entry.comments);
-  //  println!("{}", enum_entry.enum_declaration);
-  //  println!();
-  //}
-
-  println!();
-  println!("// // // // // // // // // // // // // // // // // // // // // // // // // //");
-  println!();
-
-  // Print All Group Declarations
-  let mut g: HashMap<&str, Vec<&str>> = HashMap::new();
-  for enumeration in registry.enumerations.iter().filter(|e| enums_needed.contains(e.name)) {
-    for group in enumeration.group.as_ref().unwrap_or(&"").split(',') {
-      if group == "" {
-        continue;
+fn burn_until<'a>(
+  end: &str, elem_iter: &mut impl Iterator<Item = XmlElement<'a>>,
+) {
+  loop {
+    if let EndTag { name } = elem_iter.next().unwrap() {
+      if end == name {
+        return;
       }
-      g.entry(group).or_insert(Vec::new()).push(enumeration.name);
     }
   }
-  let mut group_list: Vec<&str> = g.keys().copied().filter(|g| command_type_entries.iter().any(|cte| cte.type_declaration.contains(g))).collect();
-  group_list.sort();
-  //for group in group_list.iter() {
-  //  g.entry(group).or_insert(Vec::new()).sort();
-  //  println!("/// {}", group);
-  //  for group_entry in g.entry(group).or_insert(Vec::new()).iter() {
-  //    println!("/// * [`{}`]", group_entry);
-  //  }
-  //  println!("pub type {} = GLenum;", group);
-  //  println!("");
-  //}
-
-  println!();
-  println!("// // // // // // // // // // // // // // // // // // // // // // // // // //");
-  println!();
-
-  // Print All Command Type Declarations
-  for command_type_entry in command_type_entries.iter() {
-    // these types are crate-private and the docs for each command appear on the
-    // struct method.
-    println!("pub(crate) {}", &command_type_entry.type_declaration["pub ".len()..]);
-    println!();
-  }
-
-  println!();
-  println!("// // // // // // // // // // // // // // // // // // // // // // // // // //");
-  println!();
-  // print command loader
-  let non_null_commands: Vec<&str> = non_null_commands.iter().copied().collect();
-  let nullable_commands: Vec<&str> = nullable_commands.iter().copied().collect();
-  let mut s = String::with_capacity(10 * 1024 * 1024);
-  fmt_struct_loader(&mut s, "GlFns", &non_null_commands, &nullable_commands);
-  println!("{}", s);
 }
--- src/gl_core_types.rs
+++ src/gl_core_types.rs
@@ -1,146 +1,45 @@
-#![allow(dead_code)]
-
-use super::*;
-
-/// A GL enumeration value.
-#[derive(PartialEq, Eq, Hash)]
-#[repr(transparent)]
-pub struct GLenum(pub u32);
-impl core::fmt::Debug for GLenum {
-  fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
-    write!(f, "GlEnum(0x{:X})", self.0)
-  }
-}
-impl Clone for GLenum {
-  fn clone(&self) -> Self {
-    *self
-  }
-}
-impl Copy for GLenum {}
-
-/// A GL bitfield value.
-///
-/// You can mix around the bits of these values using standard bitwise ops.
-#[derive(PartialEq, Eq, Hash)]
-#[repr(transparent)]
-pub struct GLbitfield(pub u32);
-impl core::fmt::Debug for GLbitfield {
-  fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
-    write!(f, "GlBitfield(0b{:b})", self.0)
-  }
-}
-impl Clone for GLbitfield {
-  fn clone(&self) -> Self {
-    *self
-  }
-}
-impl Copy for GLbitfield {}
-impl core::ops::BitAnd for GLbitfield {
-  type Output = Self;
-  fn bitand(self, rhs: Self) -> Self::Output {
-    Self(self.0 & rhs.0)
-  }
-}
-impl core::ops::BitAndAssign for GLbitfield {
-  fn bitand_assign(&mut self, rhs: Self) {
-    *self = *self & rhs;
-  }
-}
-impl core::ops::BitOr for GLbitfield {
-  type Output = Self;
-  fn bitor(self, rhs: Self) -> Self::Output {
-    Self(self.0 | rhs.0)
-  }
-}
-impl core::ops::BitOrAssign for GLbitfield {
-  fn bitor_assign(&mut self, rhs: Self) {
-    *self = *self | rhs;
-  }
-}
-impl core::ops::BitXor for GLbitfield {
-  type Output = Self;
-  fn bitxor(self, rhs: Self) -> Self::Output {
-    Self(self.0 ^ rhs.0)
-  }
-}
-impl core::ops::BitXorAssign for GLbitfield {
-  fn bitxor_assign(&mut self, rhs: Self) {
-    *self = *self ^ rhs;
-  }
-}
-impl core::ops::Not for GLbitfield {
-  type Output = Self;
-  fn not(self) -> Self::Output {
-    Self(!self.0)
-  }
-}
-
-#[derive(Debug, PartialEq, Eq, Hash)]
-#[repr(transparent)]
-pub struct GLhandleARB(#[cfg(any(target_os = "ios", target_os = "macos"))] pub *mut void, #[cfg(not(any(target_os = "ios", target_os = "macos")))] pub GLuint);
-
-#[derive(Debug, PartialEq, Eq, Hash)]
-#[repr(transparent)]
-pub struct GLeglClientBufferEXT(pub *mut void);
-
-#[derive(Debug, PartialEq, Eq, Hash)]
-#[repr(transparent)]
-pub struct GLeglImageOES(pub *mut void);
-
-#[derive(Debug, PartialEq, Eq, Hash)]
-#[repr(transparent)]
-pub struct GLsync(pub *mut void);
-
-#[repr(transparent)]
-pub struct _cl_context(pub void);
-
-#[repr(transparent)]
-pub struct _cl_event(pub void);
-
-// Note(Lokathor): These type aliases are useful to end users.
-
-pub type GLDEBUGPROC = Option<unsafe extern "system" fn(source: GLenum, type_: GLenum, id: GLuint, severity: GLenum, length: GLsizei, message: *const GLchar, userParam: *const void)>;
-pub type GLDEBUGPROCARB = GLDEBUGPROC;
-pub type GLDEBUGPROCKHR = GLDEBUGPROC;
-pub type GLDEBUGPROCAMD = Option<unsafe extern "system" fn(id: GLuint, category: GLenum, severity: GLenum, length: GLsizei, message: *const GLchar, userParam: *mut void)>;
-pub type GLVULKANPROCNV = Option<unsafe extern "system" fn()>;
-
-// Note(Lokathor): These type aliases are *NOT* useful. Instead we will declare
-// them just for use within the crate. Since they aren't world-visible, the
-// outside world will show the base Rust type on function arguments.
-
-pub(crate) type GLboolean = bool;
-pub(crate) type GLbyte = i8;
-pub(crate) type GLcharARB = u8;
-pub(crate) type GLclampd = f64;
-pub(crate) type GLclampf = f32;
-pub(crate) type GLclampx = i32;
-pub(crate) type GLdouble = f64;
-pub(crate) type GLfixed = i32;
-pub(crate) type GLfloat = f32;
-pub(crate) type GLhalf = u16;
-pub(crate) type GLhalfARB = u16;
-pub(crate) type GLhalfNV = u16;
-pub(crate) type GLint = i32;
-pub(crate) type GLint64 = i64;
-pub(crate) type GLint64EXT = i64;
-pub(crate) type GLintptr = isize;
-pub(crate) type GLintptrARB = isize;
-pub(crate) type GLshort = i16;
-pub(crate) type GLsizei = u32;
-pub(crate) type GLsizeiptr = isize;
-pub(crate) type GLsizeiptrARB = isize;
-pub(crate) type GLubyte = u8;
-pub(crate) type GLuint = u32;
-pub(crate) type GLuint64 = u64;
-pub(crate) type GLuint64EXT = u64;
-pub(crate) type GLushort = u16;
-pub(crate) type GLvdpauSurfaceNV = GLintptr;
-//pub(crate) type GLvoid = void;
-pub(crate) type void = core::ffi::c_void;
-
-/// Note(Lokathor): Technically this should be `c_char` so that it's either `i8`
-/// or `u8` depending on the OS. In practice the signed-ness doesn't matter.
-/// Because Rust byte string literals (eg: `b"foo\0"`) are always `&[u8]`, we
-/// just always use `u8`, so that we can do `b"foo\0".as_ptr()`.
-pub(crate) type GLchar = u8;
+type GLenum = u32;
+type GLbitfield = u32;
+#[cfg(any(target_os = "ios", target_os = "macos"))]
+type GLhandleARB = *mut void;
+#[cfg(not(any(target_os = "ios", target_os = "macos")))]
+type GLhandleARB = GLuint;
+type GLeglClientBufferEXT = *mut void;
+type GLeglImageOES = *mut void;
+type GLsync = *mut void;
+type _cl_context = void;
+type _cl_event = void;
+type GLDEBUGPROC = Option<unsafe extern "system" fn(source: GLenum, type_: GLenum, id: GLuint, severity: GLenum, length: GLsizei, message: *const GLchar, userParam: *const void)>;
+type GLDEBUGPROCARB = GLDEBUGPROC;
+type GLDEBUGPROCKHR = GLDEBUGPROC;
+type GLDEBUGPROCAMD = Option<unsafe extern "system" fn(id: GLuint, category: GLenum, severity: GLenum, length: GLsizei, message: *const GLchar, userParam: *mut void)>;
+type GLVULKANPROCNV = Option<unsafe extern "system" fn()>;
+type GLboolean = bool;
+type GLbyte = i8;
+type GLcharARB = u8;
+type GLclampd = f64;
+type GLclampf = f32;
+type GLclampx = i32;
+type GLdouble = f64;
+type GLfixed = i32;
+type GLfloat = f32;
+type GLhalf = u16;
+type GLhalfARB = u16;
+type GLhalfNV = u16;
+type GLint = i32;
+type GLint64 = i64;
+type GLint64EXT = i64;
+type GLintptr = isize;
+type GLintptrARB = isize;
+type GLshort = i16;
+type GLsizei = u32;
+type GLsizeiptr = isize;
+type GLsizeiptrARB = isize;
+type GLubyte = u8;
+type GLuint = u32;
+type GLuint64 = u64;
+type GLuint64EXT = u64;
+type GLushort = u16;
+type GLvdpauSurfaceNV = GLintptr;
+type void = core::ffi::c_void;
+type GLchar = u8;
